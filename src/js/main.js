e62c5b992078a0dbda6ec79ab8a6b7922c1cfa02
MrMarketingMangit config --global user.email mrmarketingman001@gmail.com <mrmarketingman001@gmail.com>
2025-10-13 22:13:33 -0400

    Complete gutter banner and modal. logo.png in but not sized.

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,7 @@
 // Main Script — In Like Flynn LLC
 // =============================
 
-// Import the falling leaves animation
+
 import './animations/leaves.js';
 
-// In the future, you can import other scripts here too.
-// Example:
-// import './animations/bannerPulse.js';
-// import './animations/cursorClick.js';
-
-// This file acts as the entry point for all your site’s JS logic.
-// Right now, its only role is to run the leaf animation module.
+import './animations/modal.js';
